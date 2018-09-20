746e4dd75709e149dddbca2095e708c215f56837
jjch99 <jjch99@163.com>
2018-09-20 23:12:07 +0800

    spring-cloud-examples

--- spring-cloud-zipkin-server/src/main/java/org/example/spring/cloud/zipkin/ZipkinServerApplication.java
+++ spring-cloud-zipkin-server/src/main/java/org/example/spring/cloud/zipkin/ZipkinServerApplication.java
@@ -1,16 +1,11 @@
 package org.example.spring.cloud.zipkin;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.ZipkinServer;
 
-import zipkin.server.EnableZipkinServer;
-
-@EnableZipkinServer
-@SpringBootApplication
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ZipkinServer.main(args);
+    }
 
 }
