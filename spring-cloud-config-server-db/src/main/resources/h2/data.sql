198ee91332fbf86ae31a22daa84f51cb5bad954c
jjch99 <jjch99@163.com>
2018-10-31 17:19:01 +0800

    spring-cloud-examples

---
+++ spring-cloud-config-server-db/src/main/resources/h2/data.sql
@@ -0,0 +1,3 @@
+
+insert into system_params(param_name,param_value,comments,APPLICATION,PROFILE,LABEL)
+values('hello','Hello','注释','config-client','dev','master');
---
+++ spring-cloud-config-server-db/src/main/resources/h2/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE system_params
+(
+	param_name  VARCHAR(512),
+	param_value VARCHAR(512),
+	comments    VARCHAR(512),
+	APPLICATION VARCHAR(256),
+	PROFILE     VARCHAR(256),
+	LABEL       VARCHAR(256)
+);
