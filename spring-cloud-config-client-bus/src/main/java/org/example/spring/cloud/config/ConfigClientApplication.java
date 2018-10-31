5d1874d93f35e8f5725d6041f036f41a1af0e8dc
jjch99 <jjch99@163.com>
2018-10-31 16:04:28 +0800

    spring-cloud-examples

---
+++ spring-cloud-config-client-bus/src/main/java/org/example/spring/cloud/config/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package org.example.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-config-client-bus/src/main/java/org/example/spring/cloud/config/HelloController.java
@@ -0,0 +1,20 @@
+package org.example.spring.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloController {
+
+    @Value("${hello}")
+    private String hello;
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return hello;
+    }
+
+}
